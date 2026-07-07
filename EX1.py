f4acea4710a18a17b8747626eb3883adbd4f3fbd
Alex <alexanderbotelhopradasilva@gmail.com>
2026-07-06 23:45:32 -0300

    creation of exercise 1

---
+++ EX1.py
@@ -0,0 +1,7 @@
+# Hello World em Python
+
+
+def hello():                      #função
+    return 'Hello, World!'        #conteúdo da função
+
+print (hello())                   #printar(mostrar) o resultado da função
